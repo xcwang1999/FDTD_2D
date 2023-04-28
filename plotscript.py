1c035959d6861045d8b35efb767354dd07ba6dd7
xincheng wang <36983083+xcwang1999@users.noreply.github.com>
2023-04-28 14:52:32 +0800

    Update plotscript.py

--- plotscript.py
+++ plotscript.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits import mplot3d
 
-
-grid_row = 200
-grid_col = 200
-xv, yv = np.meshgrid(range(grid_col), range(grid_row))
-
 plt.rcParams['font.size'] = 14
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['axes.labelsize'] = 18
 plt.rcParams['font.family'] = "Times New Roman"
 plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['axes.titleweight'] = 'bold'
+plt.rcParams['axes.labelweight'] = 'bold'
 plt.rcParams['axes.linewidth'] = 2
 plt.rcParams['grid.color'] = 'gray'
 plt.rcParams['grid.linestyle'] = 'dotted'
+
+grid_row = 200
+grid_col = 200
+
+xv, yv = np.meshgrid(range(grid_col), range(grid_row))
 
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(projection="3d")
@@ -43,6 +47,27 @@ def animate(frame):
     plot_e_field(data[frame], frame)
 
 ani = animation.FuncAnimation(fig=fig, func=animate, frames=len(data), interval=20)
-ani.save("ez_cu.gif", writer="pillow")
+ani.save("ez_cu.gif", writer="pillow", )
 
 plt.show()
+
+# timestep = 2000
+# timeGPU = []
+# with open("execution_time_GPU.txt") as f:
+#     timeGPU = np.loadtxt(f)
+
+# timeCPU = []
+# with open("execution_time_CPU.txt") as f:
+#     timeCPU = np.loadtxt(f)
+
+# fig1 = plt.figure(figsize=(8, 8))
+# ax1 = fig1.add_subplot()
+# ax1.semilogy(timeGPU, label="GPU")
+# ax1.semilogy(timeCPU, label="CPU")
+# ax1.set_xlim(0, timestep)
+# ax1.set_xlabel("timestep")
+# ax1.set_ylabel("execution time(s)")
+# ax1.legend()
+# ax1.set_title("Time required for each loop")
+# plt.savefig("time_comparison.png")
+# plt.show()
